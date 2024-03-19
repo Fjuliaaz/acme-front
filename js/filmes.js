686a8ad53e70262e583c1c096939666a272ce738
JULIA APARECIDA DA SILVA FONSECA <23111808@CFP127ALUNO.LOCAL>
2024-03-19 11:49:58 -0300

    finalizando

--- js/filmes.js
+++ js/filmes.js
@@ -9,5 +9,19 @@ export async function getFilmes (id){
     const url = `http://10.107.134.43:8080/v2/acme/filme/${id}`
     const response = await fetch(url)
     const data = await response.json()
-    console.table(data)
+    return data.filme[0]
+}
+
+export async function postFilme (filme) {
+    const url = 'http://10.107.134.43:8080/v2/acmefilmes/filme'
+    const options = {
+        method: 'POST',
+        Headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(filme)
+    }
+     const response = await fetch (url, options)
+
+     return response.ok
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
-import { getFilmes } from "./filmes";
+import { getFilmes, getFilme  } from "./filmes.js";
 
-console.table(await getFilmes())
+function criarCard(filme){
+    const card = document.createElement('div')
+    const titulo = document.createElement('h2')
+    titulo.textContent = filme.nome
+
+    const valor = document.createElement('button')
+    valorButton.textContent = filme.valor_unitario
+
+    card.appendChild(titulo, valor)
+    return card
+}
+
+async function preencherContainer(){
+    const container = document.querySelector('body')
+    const filmes = await getFilmes()
+    
+    filmes.forEach( filme => { 
+        const card = criarCard(filme)
+        container.appendChild (card)
+    })
+    
+}
+
+preencherContainer()
+
+filme = {
+    "nome": "O Segredo do Vale",
+    "sinopse": "Um drama emocionante que explora os segredos de uma pequena cidade no interior.",
+    "duracao": "2 horas e 15 minutos",
+    "data_lancamento": "2022-08-25",
+    "data_relancamento": null,
+    "foto_capa": "https://exemplo.com/foto_vale.jpg",
+    "valor_unitario": 29.99
+}
+
+console.table(await getFilmes(1))
